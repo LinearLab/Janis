970bf1fd15ca7ff05d1cb5dd2c9e40f02ebe83e1
Humberto Kramm <humbertokramm@gmail.com>
2018-06-15 19:08:38 -0300

    teste prático

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,28 +2,28 @@
   Este programa liga o motor por 1 segundo, toda
   vez que recebe o caractere ASCCII "1" pela porta
   serial. A Velocidade da Porta Serial foi
-  definida em 9600bps.
+  definida em 115200bps.
 */
 
 int Value = 0;
+int MOTOR = D4;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  pinMode(2, OUTPUT);
+  pinMode(MOTOR, OUTPUT);
 }
 
 void loop()
 {
   if (Serial.available() == 1) {
     Value = Serial.read();
-    // Faremos o teste com o numero 49, que representa
-    // "1" em ASCII
-    if (Value == 49) {
-      digitalWrite(2, HIGH);
-      delay(1000); // Wait for 1000 millisecond(s)
-      digitalWrite(2, LOW);
+    // Faremos o teste com o numero "1" em ASCII
+    if (Value == '1') {
+      digitalWrite(MOTOR, HIGH);
+      delay(500); // Wait for 1000 millisecond(s)
+      digitalWrite(MOTOR, LOW);
     }
   }
 }
