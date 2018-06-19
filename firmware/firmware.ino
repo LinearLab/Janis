43cbba1903ca7c26e46365d45bcdd801350d514c
Humberto Kramm <humbertokramm@gmail.com>
2018-06-19 15:23:26 -0300

    Bug do restart
    Fui fazer mais uns testes de outra função hoje e percebi um problema. Toda vez que ligava o sistema, tinha que desligar o motor antes, pois a plaquinha ficava com a serial bugada.
    
    Já resolvi isso e fiz as alterações necessárias no código.
    
    Além disso, acrescentei um Led para indicar se o sistema não travou.
    
    Esse led vai dar uma piscada rápida a cada 1 segundo indicando que o sistema está rodando e quando o Motor efetuar um disparo, o mesmo led vai ligar, mas por um tempinho mais longo.
    
    Coloquei também um feedback na serial e baixei a velocidade, mas pode aumentar depois.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,28 +2,52 @@
   Este programa liga o motor por 1 segundo, toda
   vez que recebe o caractere ASCCII "1" pela porta
   serial. A Velocidade da Porta Serial foi
-  definida em 115200bps.
+  definida em 9600bps.
 */
-
-int Value = 0;
-int MOTOR = D4;
-
+char Value = 0;
+int MOTOR = D8;
+int LED = D4;
+long int count;
+#define TEMPO_OFF 116279  //~1 segundo
+#define TEMPO_ON  100 //840us
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
+  Serial.println("Sistema Ligado");
   pinMode(MOTOR, OUTPUT);
+  pinMode(LED, OUTPUT);
+
+  digitalWrite(MOTOR, LOW);
+  digitalWrite(LED, HIGH);
 }
 
 void loop()
 {
-  if (Serial.available() == 1) {
+  if (Serial.available()) 
+  {
+    
     Value = Serial.read();
     // Faremos o teste com o numero "1" em ASCII
-    if (Value == '1') {
+    if (Value == '1') 
+    {
       digitalWrite(MOTOR, HIGH);
+      digitalWrite(LED, LOW);
+      Serial.println("Disparo Executado");
       delay(500); // Wait for 1000 millisecond(s)
+      digitalWrite(LED, HIGH);
       digitalWrite(MOTOR, LOW);
+      Value = 0;
+      
     }
   }
+  count++;
+  if(count == TEMPO_OFF) digitalWrite(LED, LOW);
+  else if(count == TEMPO_ON+TEMPO_OFF) 
+  {
+    digitalWrite(LED, HIGH);
+    count = 0;
+  }
+  
+  //delay(100);
 }
